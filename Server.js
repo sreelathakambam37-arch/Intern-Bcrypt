6a7dd198abab44a2be59afb44df4a56cc4826268
Kambam Sreelatha <sreelathakambam37@gmail.com>
2026-07-14 17:25:37 +0530

    bcrypt

--- Server.js
+++ Server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const User = require('./model');
+const Todo = require('./Todo'); 
+const UserData = require('./model'); 
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
@@ -9,72 +10,166 @@ const app = express();
 
 app.use(express.json());
 
-mongoose.connect('').then(
+mongoose.connect('mongodb+srv://sreelathakambam37_db_user:sTXQ1enPc5YssUxO@cluster0.e4wcv65.mongodb.net/').then(
     ()=> console.log('connected to database.......')
 ).catch(err=>console.log(err))
 
-app.post('/signup', async (req,res)=>{
-    const {username}= req.body
-    const {email}= req.body
-    const {password}= req.body
-    try{
-        if (!email || !password || !username) {
-            return res.json({message:"username,email and password are required"});
-        }
-         //hash password
-         const salt = await bcrypt.genSalt(10);
-         const hashed_password =await bcrypt.hash(password,salt);
+const authenticateToken = (req, res, next) => {
 
-         const newUser = new User({username,email,password:hashed_password})
-         await newUser.save();
-         const token = jwt.sign({id:newUser._id},'this is my secret key',{expiresIn:'1h'});   
-         return res.status(200).json({message:"user signup successfully",
+    const authHeader = req.headers.authorization;
+
+    const token = authHeader && authHeader.split(" ")[1];
+
+    if (!token) {
+        return res.status(401).json({
+            message: "Access token required"
+        });
+    }
+
+    jwt.verify(token, "this is my secret key", (err, user) => {
+
+        if (err) {
+            return res.status(403).json({
+                message: "Invalid or expired token"
+            });
+        }
+        req.user = user;
+        next();
+    });
+
+};
+
+
+app.post("/signup", async (req,res)=>{
+    const {username, email, password} = req.body
+    try{
+        const existingUser = await UserData.findOne({email})
+        if (existingUser){
+            return res.status(400).json({ message: "User already exists" });
+        }
+        const salt =  await  bcrypt.genSalt(10);
+        const hashed_password = await bcrypt.hash(password, salt);
+        const newUser = new UserData({username, email, password : hashed_password});
+        await newUser.save();
+        const token = jwt.sign({id : newUser._id},'this is my secret key',{expiresIn:'1h'} );
+        return res.json({
+            message: "User signup successful",
             token,
             user:{
                 id:newUser._id,
-                username:newUser.username,
-                email:newUser.email
+                username : newUser.username,
+                email: newUser.email
             }
-         });
-        
-    } 
-    catch(err){
-        console.log(err.message)
+        });
     }
-} )
-
-app.post("/login", async (req,res)=>{
-    const {email}=req.body
-    const {password}=req.body
-    try{
-        if (!email || !password) {
-            return res.json({message:"Email and password are required"});
-        }
-        
-        const findUser = await User.findOne({email});
-        if (!findUser) {
-            return res.json({message:"user not found"});
-        }
-
-        const isMatch = await bcrypt.compare(password,findUser.password)
-        if(!isMatch){
-          return res.json({message:"invalid password"});
-        }
-        const token = jwt.sign({id:findUser._id},'this is my secret key',{expiresIn:'1h'});
-        return res.json({
-            message: "login successfully",
-            token:token,
-            user:{
-                id:findUser._id,
-                username:findUser.username,
-                email:findUser.email
-                
-            }
-        })
-    }
-    catch(err){
+    catch (err){
         console.log(err.message)
+        return res.status(500).json({ message: "Server error" });
     }
 })
+
+
+app.post("/login" , async (req,res) =>{
+    try{
+        const {email,password} = req.body
+        const foundUser = await UserData.findOne({ email });
+        if (!foundUser){
+            return res.status(401).json({ message: "Invalid credentials" })
+        }
+    
+        console.log(password);
+        console.log(foundUser);
+        console.log(foundUser.password);
+        console.log(req.body);
+        const ismatch = await bcrypt.compare(password, foundUser.password);
+        if (!ismatch){
+          return res.status(401).json({ message: "Invalid password" })
+
+        }
+        const token = jwt.sign({id : foundUser._id}, "this is my secret key" ,{expiresIn :'1h'});
+        return res.status(201).json({
+            message:"user login successful",
+            token,
+            user:{
+                id : foundUser._id,
+                username: foundUser.username,
+                email: foundUser.email
+            }
+        });
+    }
+    catch (err){
+          console.log(err.message);
+        return res.status(500).json({ message: "Server error" });
+    }
+})
+
+app.post("/create_task", authenticateToken , async (req,res) => {
+   try{
+    const {title,description} = req.body;
+    const newTODO = new ToDo({title,description,userId: req.user.id} );
+    await newTODO.save();
+    return res.status(201).json({
+        message: "TODO created successfull",
+        Todo:  newTODO
+    });
+   }
+    catch (err){
+          console.log(err.message);
+        return res.status(500).json({ message: "Server error" });
+    }
+});
+app.get('/todos', authenticateToken, async (req, res) => {
+    try {
+        const todos = await ToDo.find({ userId: req.user.id }).sort({ createdAt: -1 }); 
+        return res.status(200).json({
+            message: 'Todos fetched successfully',
+            todos
+        });
+    } catch (err) {
+        console.error('Todo fetch error:', err);
+        return res.status(500).json({ 'message': 'Server Error' });
+    }
+});
+
+app.put('/todo/:id', authenticateToken, async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { title, description } = req.body;
+        const todo = await ToDo.findOne({ _id: id, userId: req.user.id });
+        if (!todo) {
+            return res.status(404).json({ 'message': 'Todo not found or access denied' });
+        }
+        todo.title = title || todo.title;
+        todo.description = description || todo.description;
+        await todo.save();
+        return res.status(200).json({
+            message: 'Todo updated successfully',
+            todo
+        });
+    } catch (err) {
+        console.log(err.message);
+        
+    }
+});
+
+
+app.delete('/todo/:id', authenticateToken, async (req, res) => {
+    try {
+        const {id}= req.params;
+        const todo = await ToDo.findOneAndDelete({_id:id, userId: req.user.id})
+        if(!todo){
+            return res.json({'message':'todo not found'})
+        }
+        return res.json({
+            message:'todo deleted successfully',
+            todo
+        });
+    }catch (err) {
+        console.log(err.message);
+    }
+});
+
+
+
 
 app.listen(3000,()=>console.log('server running on http://127.0.0.1:3000.....'))
